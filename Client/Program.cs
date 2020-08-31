cb2cd1a74677b807919db0b575da523cfaa88482
Demmon <dima.521.shvab@gmail.com>
2020-08-31 13:57:37 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+
+        static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Change what do u want:\n" +
+                    "1 - add new line\n" +
+                    "2 - delete last line\n" +
+                    "3 - add new station\n" +
+                    "4 - delete last station\n" +
+                    "5 - change last station\n" +
+                    "6 - search station on \"newItem\" name\n" +
+                    "7 - count of station last line\n" +
+                    "8 - list of station last line\n" +
+                    "9 - full list of lines\n" +
+                    "0 - exit");
+
+            socket.Connect(ipPoint);
+
+            RunClient();
+        }
+
+        static void RunClient()
+        {
+            while (true)
+            {
+                int answer = int.Parse(Console.ReadLine());
+
+                if (answer == 0)
+                    break;
+
+                byte[] data = BitConverter.GetBytes(answer);  // отправляем результат
+                socket.Send(data);
+
+                byte[] newData = new byte[512];
+                socket.Receive(newData);
+                string serverAnsver = Encoding.Unicode.GetString(newData);
+
+                Console.WriteLine(serverAnsver);
+            }
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static int port = 8005; // порт для приема входящих запросов
+
+        // получаем адреса для запуска сокета
+        static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+        // создаем сокет
+        static Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static Socket handler1;
+
+        static int ansver;
+
+        static void Main(string[] args)
+        {
+            RunServer();
+        }
+
+        static public void RunServer()
+        {
+            // связываем сокет с локальной точкой, по которой будем принимать данные
+            listenSocket.Bind(ipPoint);
+
+            listenSocket.Listen(10);
+
+            handler1 = listenSocket.Accept();
+
+            if (!handler1.Connected)
+                throw new Exception("Не удалось подключится к клиентам");
+            else
+                Console.WriteLine("Подключено");
+
+
+            while (true)
+            {
+                byte[] data = new byte[256];
+
+                handler1.Receive(data);
+                ansver = BitConverter.ToInt32(data, 0);
+
+                Model model = new Model();
+
+                switch (ansver)
+                {
+                    case 1:
+                        model.UndergroundLines.Add(new UndergroundLine("newItem", new List<Station>() { new Station() }, 1));
+                        model.SaveChanges();
+
+                        handler1.Send(Encoding.Unicode.GetBytes($"New line added!"));
+                        Console.WriteLine(1);
+                        break;
+                    case 2:
+                        if (model.UndergroundLines.Count() > 0)
+                        {
+                            var index = model.UndergroundLines.Count() - 1;
+                            var remItem = model.UndergroundLines.ToList()[index];
+
+                            foreach (var item in model.Stations)
+                            {
+                                model.Stations.Remove(item);
+                            }
+
+                            model.UndergroundLines.Remove(remItem);
+
+                            model.SaveChanges();
+
+                            handler1.Send(Encoding.Unicode.GetBytes($"Last line deleted!"));
+                        }
+                        else
+                            handler1.Send(Encoding.Unicode.GetBytes($"Lines empty!"));
+
+                        Console.WriteLine(2);
+                        break;
+                    case 3:
+                        model.Stations.Add(new Station("newItem", 1, 1));
+                        model.SaveChanges();
+
+                        handler1.Send(Encoding.Unicode.GetBytes($"New station added!"));
+
+                        Console.WriteLine(3);
+                        break;
+                    case 4:
+                        if (model.Stations.Count() > 0)
+                        {
+                            var index = model.Stations.Count() - 1;
+                            var remItem = model.Stations.ToList()[index];
+
+                            model.Stations.Remove(remItem);
+
+                            model.SaveChanges();
+
+                            handler1.Send(Encoding.Unicode.GetBytes($"Last station deleted!"));
+                        }
+                        else
+                            handler1.Send(Encoding.Unicode.GetBytes($"Stations empty!"));
+
+                        Console.WriteLine(4);
+                        break;
+                    case 5:
+                        if (model.Stations.Count() > 0)
+                        {
+                            var index = model.Stations.Count() - 1;
+                            var lastStation = model.Stations.ToList()[index];
+
+                            lastStation.Name = "changedName";
+                            lastStation.SomeProp1 = 2;
+                            lastStation.SomeProp2 = 2;
+
+                            model.SaveChanges();
+
+                            handler1.Send(Encoding.Unicode.GetBytes($"Last station changed!"));
+                        }
+                        else
+                            handler1.Send(Encoding.Unicode.GetBytes($"Stations empty!"));
+
+                        Console.WriteLine(5);
+                        break;
+                    case 6:
+                        if (model.Stations.Count() > 0)
+                        {
+                            var station = model.Stations.First(x => x.Name == "newItem");
+
+                            handler1.Send(Encoding.Unicode.GetBytes(station.ToString()));
+                        }
+                        else
+                            handler1.Send(Encoding.Unicode.GetBytes($"Stations empty!"));
+
+                        Console.WriteLine(6);
+                        break;
+                    case 7:
+                        if (model.UndergroundLines.Count() > 0)
+                        {
+                            model.Stations.Load();
+
+                            var index = model.UndergroundLines.Count() - 1;
+                            var line = model.UndergroundLines.ToList()[index];
+
+                            handler1.Send(Encoding.Unicode.GetBytes(line.Stations.Count.ToString()));
+                        }
+                        else
+                            handler1.Send(Encoding.Unicode.GetBytes($"Lines empty!"));
+
+                        Console.WriteLine(7);
+                        break;
+                    case 8:
+                        if (model.UndergroundLines.Count() > 0)
+                        {
+                            var index = model.UndergroundLines.Count() - 1;
+                            var line = model.UndergroundLines.ToList()[index];
+
+                            string stations = null;
+
+                            foreach (var item in line.Stations)
+                            {
+                                stations += item.ToString();
+                            }
+
+                            handler1.Send(Encoding.Unicode.GetBytes(stations));
+                        }
+                        else
+                            handler1.Send(Encoding.Unicode.GetBytes($"Lines empty!"));
+
+                        Console.WriteLine(8);
+                        break;
+                    case 9:
+                        if (model.UndergroundLines.Count() > 0)
+                        {
+                            string lines = null;
+
+                            foreach (var item in model.UndergroundLines)
+                            {
+                                lines += item.ToString();
+                            }
+
+                            handler1.Send(Encoding.Unicode.GetBytes(lines));
+                        }
+                        else
+                            handler1.Send(Encoding.Unicode.GetBytes($"Lines empty!"));
+
+                        Console.WriteLine(9);
+                        break;
+                    default:
+                        handler1.Send(Encoding.Unicode.GetBytes($"Invalid operation!"));
+                        break;
+                }
+            }
+        }
+    }
+}
